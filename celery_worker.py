414d8406fa5af1f099a7ea36be65e6686ba335eb
twaasoulElm3refa <twaslm3rfa@gmail.com>
2025-08-03 09:34:59 +0300

    Delete celery_worker.py

--- celery_worker.py
+++
@@ -1,8 +0,0 @@
-from celery import Celery
-import os
-
-# Initialize Celery app with Redis broker
-celery = Celery("tasks", broker=os.getenv("REDIS_URL"))
-
-# Import tasks from tasks.py (task functions)
-from tasks import process_tool
